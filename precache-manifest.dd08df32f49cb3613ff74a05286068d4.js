4481b57af9afcdd23c98cc3b348e448d2db1f78e
Николай Шкрылев <snnwolf@gmail.com>
2020-02-16 23:44:37 +0300

    Updates

---
+++ precache-manifest.dd08df32f49cb3613ff74a05286068d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfc0446b7e0a7e4e8e78413baa9d8f9",
+    "url": "/ergo-wbo/index.html"
+  },
+  {
+    "revision": "c510314ceb3481c5977a",
+    "url": "/ergo-wbo/static/css/2.79f74de8.chunk.css"
+  },
+  {
+    "revision": "f870ea39b458a8364254",
+    "url": "/ergo-wbo/static/css/main.3cd639d4.chunk.css"
+  },
+  {
+    "revision": "c510314ceb3481c5977a",
+    "url": "/ergo-wbo/static/js/2.f08312b5.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/ergo-wbo/static/js/2.f08312b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f870ea39b458a8364254",
+    "url": "/ergo-wbo/static/js/main.6e09408f.chunk.js"
+  },
+  {
+    "revision": "baea60bf7ce4f5f0b00c",
+    "url": "/ergo-wbo/static/js/runtime-main.a807ec4f.js"
+  }
+]);
